155f0af5018f9e62e55a75b6f9722d591f7f7120
山本 啓太 <keita.yamamoto.1009@gmail.com>
2023-04-06 17:53:45 +0900

    20230406

---
+++ src/main/java/com/kmountain/comp_tool_manager/CompToolManagerApplication.java
@@ -0,0 +1,13 @@
+package com.kmountain.comp_tool_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompToolManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompToolManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kmountain/comp_tool_manager/CompToolManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmountain.comp_tool_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompToolManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

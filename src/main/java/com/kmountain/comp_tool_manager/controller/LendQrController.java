b27fde63d5c4630d095227230b785587a17466ed
山本 啓太 <keita.yamamoto.1009@gmail.com>
2023-08-07 18:20:24 +0900

    20230807

--- src/main/java/com/kmountain/comp_tool_manager/controller/LendQrController.java
+++ src/main/java/com/kmountain/comp_tool_manager/controller/LendQrController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.kmountain.comp_tool_manager.form.SearchForm;
+import com.kmountain.comp_tool_manager.form.LendQrForm;
 import com.kmountain.comp_tool_manager.service.CategoryService;
 
 @Controller
@@ -23,13 +23,19 @@ public class LendQrController {
 
 	@RequestMapping(value = "", method = RequestMethod.GET)
 	public ModelAndView get(ModelAndView mav) {
+		LendQrForm qrform = new LendQrForm();
+
+		mav.addObject("qrform", qrform);
 		mav.setViewName("lendqr");
 
 		return mav;
 	}
 
 	@RequestMapping(value = "", method = RequestMethod.POST)
-	public ModelAndView post(ModelAndView mav, SearchForm search_form) {
+	public ModelAndView post(ModelAndView mav, LendQrForm qrform) {
+		String result = "";
+
+		mav.addObject("result", result);
 		mav.setViewName("lendqr");
 
 		return mav;
---
+++ src/main/java/com/kmountain/comp_tool_manager/form/LendQrForm.java
@@ -0,0 +1,14 @@
+package com.kmountain.comp_tool_manager.form;
+
+import lombok.Data;
+
+@Data
+public class LendQrForm {
+
+	private String itemNumber;
+
+	private String user;
+
+	private String lendcat;
+
+}
